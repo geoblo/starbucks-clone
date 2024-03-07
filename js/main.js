99a5b866371180b134a99dfb21ebaffe08e3711d
geoblo <geoblo@naver.com>
2024-03-07 12:48:20 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -23,3 +23,37 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.removeAttribute('placeholder');
   searchEl.classList.remove('focused');
 });
+
+// 스크롤 시 전역 배지(고정 배너) 숨기기
+const badgeEl = document.querySelector('header .badges');
+
+// 페이지에 스크롤 이벤트 감지를 추가!
+// window: 브라우저 창 객체
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY); // (y축으로 얼마나 스크롤 했는지) 페이지 스크롤 위치
+
+  // Quiz:
+  // 페이지 스크롤 위치가 500px을 넘으면 배지 요소를 숨기고,
+  // 페이지 스크롤 위치가 500px을 넘지 않으면 배지 요소 보이기!
+  // 힌트: style.display = 'none';
+  if (window.scrollY > 500) {
+    // badgeEl.style.display = 'none';
+    // badgeEl.style.opacity = 0;
+    // badgeEl.style.visibility = 'hidden';
+
+    // gsap.to(요소, 지속시간, 옵션: {}) 메소드: CSS 속성을 통해 애니메이션 처리
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // badgeEl.style.display = 'block';
+    // badgeEl.style.opacity = 1;
+    // badgeEl.style.visibility = 'visible';
+
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+});
